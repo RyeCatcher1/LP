0f12be80d1ea2f98dbefe78972ff5c8371f043f7
mateo <mateo.hidalgo@usm.cl>
2024-05-12 22:48:09 -0400

    añadi algunas cosas a las clases

--- src/Arena.java
+++ src/Arena.java
@@ -1,3 +1,32 @@
 public class Arena {
+    private int ronda;
+    private int tamano;
     
+
+    public void mostrar(){
+
+    }
+    public void nuevaRonda(){
+
+    }
+
+    public int get_ronda(){
+        return this.ronda;
+    }
+
+    public void set_ronda(int ronda){
+        this.ronda=ronda;
+
+    }
+
+    public int get_tamano(){
+        return this.tamano;
+    }
+
+    public void set_tamano(int tamano){
+        this.tamano=tamano;
+
+    }
+
+
 }
